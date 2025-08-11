946e3e3d270d85c6d4d1bf7a256e95a41e85cbab
Shambhav kumar rao <94632497+Kumarshambhav@users.noreply.github.com>
2025-08-11 17:03:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -68,7 +68,7 @@ app.post("/api/register", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: false, // set true in production (HTTPS)
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000
     });
 
